c194bc82d28e3195498f79dd3a5229477fda1528
kiwios-env <kiwios@proton.me>
2026-09-07 14:57:42 -0400

    added doxygen comment to helloWorld.cpp

--- 000/helloWorld.cpp
+++ 000/helloWorld.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+/**
+ * @param argc The number of command line arguments.
+ * @param argv The command line arguments.
+ */
 int main(int argc, char *argv[]) {
 	std::string name;
 	std::cout << "Hello, what is your name? ";
